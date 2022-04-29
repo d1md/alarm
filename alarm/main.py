7c78763dec752ba587d67545887dd1efea1517f4
pokendau <daniplingau@gmail.com>
2022-04-29 14:47:57 +0300

    Finished first prototype

--- alarm/main.py
+++ alarm/main.py
@@ -1,5 +1,16 @@
 from datetime import *
-
+from playsound import playsound
 hour = int(input("Give me the hour"))
 minute = int(input("Give me the minute"))
 second = int(input("Give me the seconds"))
+day = input("Pm or am")
+running = True
+
+if day == "pm":
+    hour += 12
+
+while running:
+    if datetime.now().hour == hour and datetime.now().minute == minute and datetime.now().second == second:
+        running = False
+playsound('D:\\Programare\\Python\\repositories\\alarm\\alarm\\alarm\\alarm-sound.mp3')
+
